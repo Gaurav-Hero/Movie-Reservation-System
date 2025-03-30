6b000b4ce4c52ba0b054a1d16a0539758365010e
Gaurav Sharma <gs236934@gmail.com>
2025-03-30 10:23:17 +0530

    gaurav - route upload

--- backend/routes/movie.routes.js
+++ backend/routes/movie.routes.js
@@ -1,1 +1,10 @@
- 
+   
+import express from "express";
+import { createMovie, getAllMovies, getMovieById, updateMovie, deleteMovie } from "../services/movieService/movie.controller";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+export default router;
